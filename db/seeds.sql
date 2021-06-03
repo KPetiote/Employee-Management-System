7155f3a411d5a67d47d666d2a334ffd94dba4d8b
Kenneth Petiote <Ken@Kens-iMac.local>
2021-06-02 23:28:43 -0400

    added role information

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,36 +4,32 @@ USE employee_management_system_db;
 -- DEPARTMENTS
 -- Inserts department information into department table. 
 INSERT INTO department (name) VALUES
-    (Accessioning)
-    (Accounting)
-    (Billing)
-    (Chemistry)
-    (Cytology)
-    (Hematology)
-    (Human Resources)
-    (Immunology)
-    (Information Technology)
-    (Management)
-    (Microbiology)
-    (Phlebotomy)
-    (Sales)
+    (Accessioning) /*1*/
+    (Accounting) /*2*/
+    (Billing) /*3*/
+    (Laboratory) /*4*/
+    (Human Resources) /*5*/
+    (Information Technology) /*6*/
+    (Management) /*7*/
+    (Phlebotomy) /*8*/
+    (Sales) /*9*/
 
 -- ROLES
 -- Inserts role information into role table. 
 INSERT INTO role (title, salary, department_id) VALUES
-    ("Accountant", 50000, 1),
-    ("Billing Specialist", 40000, 2),
-    ("Chemical Technologists", 50000, 3),
+    ("Accountant", 50000, 2),
+    ("Billing Specialist", 40000, 3),
+    ("Billing Manager", 85000, 3),
+    ("Medical Technologists", 50000, 4),
     ("HR Manager",),
-    ("Immunology Technician",),
     ("IT Manager",),
     ("IT Techncian",),
-    ("Laboratory Director", 200000, 10)
-    ("Laboratory Manager",),
-    ("Laboratory Supervisor", 80000,)
-    ("Phlebotomist,",),
-    ("Sales Representative",),
-    ("Specimen Accessioner", 30000, 0),
+    ("Laboratory Director", 200000, 7),
+    ("Laboratory Manager", 85000, 7),
+    ("Laboratory Supervisor", 80000, 4),
+    ("Phlebotomist," 35000, 8),
+    ("Sales Representative", 55000, 9),
+    ("Specimen Accessioner", 30000, 1),
 
 
 
