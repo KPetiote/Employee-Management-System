2512fab1835f3847c2983fa2bc563e0398adcf5c
Kenneth Petiote <Ken@Kens-iMac.local>
2021-06-02 22:08:40 -0400

    added role information

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,17 +2,39 @@
 USE employee_management_system_db;
 
 -- DEPARTMENTS
--- Inserts department information to department table. 
+-- Inserts department information into department table. 
 INSERT INTO department (name) VALUES
-(Accessioning)
-(Accounting)
-(Billing)
-(Chemistry)
-(Cytology)
-(Hematology)
-(Human Resources)
-(Immunology)
-(Information Technology)
-(Microbiology)
-(Phlebotomy)
-(Sales)
+    (Accessioning)
+    (Accounting)
+    (Billing)
+    (Chemistry)
+    (Cytology)
+    (Hematology)
+    (Human Resources)
+    (Immunology)
+    (Information Technology)
+    (Management)
+    (Microbiology)
+    (Phlebotomy)
+    (Sales)
+
+-- ROLES
+-- Inserts role information into role table. 
+INSERT INTO role (title, salary, department_id) VALUES
+    ("Accountant", 50000, 1),
+    ("Billing Specialist", 40000, 2),
+    ("Chemical Technologists", 50000, 3),
+    ("HR Manager",),
+    ("Immunology Technician",),
+    ("IT Manager",),
+    ("IT Techncian",),
+    ("Laboratory Director", 200000, 10)
+    ("Laboratory Manager",),
+    ("Laboratory Supervisor", 80000,)
+    ("Phlebotomist,",),
+    ("Sales Representative",),
+    ("Specimen Accessioner", 30000, 0),
+
+
+
+
