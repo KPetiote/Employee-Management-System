3487b649aeef43464ece08cd90ba34bf2404e921
Kenneth Petiote <Ken@Kens-iMac.local>
2021-06-02 21:41:14 -0400

    added departement information to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,15 @@
 -- Makes it so all of the following code will affect the employee_management_system_db" database.
 USE employee_management_system_db;
 
-INSERT INTO department(name) VALUES
+-- Inserts department information.
+INSERT INTO department (name) VALUES
+(Accessioning)
+(Billing)
+(Chemistry)
+(Cytology)
+(Hematology)
+(Immunology)
+(IT)
+(Microbiology)
+(Phlebotomy)
+(Sales)
